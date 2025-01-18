497795fe0b7c86c917b267f38e254c53c0f689d1
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-18 13:49:33 -0700

    add new numbers & string variables ruby file

---
+++ variables/numbers_and_string_variables.rb
@@ -0,0 +1,43 @@
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+
+number = 1
+
+puts number + 10
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+word = "hello"
+
+puts word.upcase
+
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+number1 = 20
+number2 = 31
+
+puts number1 + number2
+
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+word = "Goodbye"
+
+puts word.reverse
+
+# Write a program that uses a variable to store a number, then prints the number times 10.
+
+number = 10
+
+puts number * 10
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = "good morning"
+word2 = "good afternoon"
+
+puts word1.upcase + ", and " + word2.upcase
+
+# Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+word = "somebody"
+
+puts word.length
